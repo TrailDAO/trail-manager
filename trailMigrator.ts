ef08e35a523dbe88640cb03364ca74dfabaa7f04
PapaPizzaiolo <100707192+PapaPizzaiolo@users.noreply.github.com>
2022-05-17 16:32:53 -0500

    Placeholder for migrator

---
+++ trailMigrator.ts
@@ -0,0 +1,9 @@
+// crawl nrtdatabase website
+// call trailmix-manager api and create location
+// status becomes trail created, compiled, and deployed
+
+// create api calls to search for trails nearest your current location
+// sort by lat / long and try filters by post calculated distance
+
+// also need a migrator for pulling stuff off of americantrails.org
+// nrt database
